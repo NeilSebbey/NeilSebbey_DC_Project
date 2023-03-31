6307e9efcccefc3e3a4bcd8a0e740a074e9d52a8
Neil Sebbey <neil.p.sebbey@students.ittralee.ie>
2023-03-31 15:02:44 +0100

    Final commit before submission.

--- src/client/SMPClient.java
+++ src/client/SMPClient.java
@@ -40,7 +40,7 @@ public class SMPClient extends JFrame {
          boolean done = false;
          boolean vLogin;
          boolean loggedIn = false;
-         String message, echo, uName, pWord;
+         String message, echo;
 
 
          while (!done) {
@@ -128,22 +128,5 @@ public class SMPClient extends JFrame {
          ex.printStackTrace( );
       } //end catch
    } //end main
-
-   private static void printSocketInfo(SSLSocket s) {
-      System.out.println("Socket class: "+s.getClass());
-      System.out.println("   Remote address = "
-              +s.getInetAddress().toString());
-      System.out.println("   Remote port = "+s.getPort());
-      System.out.println("   Local socket address = "
-              +s.getLocalSocketAddress().toString());
-      System.out.println("   Local address = "
-              +s.getLocalAddress().toString());
-      System.out.println("   Local port = "+s.getLocalPort());
-      System.out.println("   Need client authentication = "
-              +s.getNeedClientAuth());
-      SSLSession ss = s.getSession();
-      System.out.println("   Cipher suite = "+ss.getCipherSuite());
-      System.out.println("   Protocol = "+ss.getProtocol());
-   }
 
 } // end class
